d03b7590a6533abde0f162017b4c48265df192ee
ialexies <ialexies@gmail.com>
2018-08-29 00:51:19 +0800

    home, films, thecrew filters
    Add filters for films
    Add the show data of directors, cast, producers,
    Add search functionality for navbar global

---
+++ database/seeds/ProjectTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Project;
+use Illuminate\Database\Seeder;
+
+class ProjectTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Project::class,10)->create();
+    }
+}
